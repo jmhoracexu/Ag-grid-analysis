b833bcfed6c1b6c6ee451402e533f2bec487b5fc
Winterbottle <j.m.horacexu@gmail.com>
2024-02-21 11:30:41 +0800

    first commit.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,11 +14,11 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'Ag-grid' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('Ag-grid');
-  });
+  // it(`should have the 'Ag-grid' title`, () => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   const app = fixture.componentInstance;
+  //   expect(app.title).toEqual('Ag-grid');
+  // });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { AgGridAngular } from 'ag-grid-angular';
+
+
+import { ColDef } from 'ag-grid-community';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [AgGridAngular],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'Ag-grid';
+export class AppComponent implements OnInit{
+  
+  accountList:any[] = []
+  constructor(private http: HttpClient){
+
+  }
+  ngOnInit(): void {
+    this.getAccount()
+  }
+
+  getAccount(){
+    this.http.get('https://raw.githubusercontent.com/a-tremblay/grid-poc/main/data/accounts.json').subscribe((res:any)=> {
+      this.accountList = res
+    })
+  }
+
+  colDefs: ColDef[] = [
+    { field: "_id" },
+    { field: "name" },
+    { field: "timezone" },
+    { field: "currency" }
+  ]
 }
